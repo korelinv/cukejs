9a3208c518a3c5bf53f03d89ce66ce3237c7aaef
Korelinv <trickfactory1@gmail.com>
2017-09-19 00:29:48 +0300

    testbulder prototype prettyfy

--- index.js
+++ index.js
@@ -1,61 +1,6 @@
 const featureParser = require('./lib/featureParser');
+const testBuilder = require('./lib/testBuilder');
 
-
-function testBuilder(feature)
-{
-
-    let scenarios = feature.scenario.map((scenario) => {
-        let tags = feature.tags;
-        let name = `${feature.name} ${feature.background.name} ${scenario.name}`.trim();
-        let content = feature.background.content.concat(scenario.content);
-        return {tags, name, content};
-    });
-
-    let outlines = feature.outline
-        .map((outline) => {
-            let tags = feature.tags;
-            let name = `${feature.name} ${feature.background.name} ${outline.name}`.trim();
-
-            let tableOfContent = {};
-            let tableOfContentLength = 0;
-            let variablesLookup = {};
-
-            outline.tableOfContent.shift()
-            .forEach((variableName, index) => {
-                variablesLookup[index] = variableName;
-                tableOfContent[variableName] = [];
-            });
-
-            outline.tableOfContent.forEach((row) => {
-                tableOfContentLength++;
-                row.forEach((element, index) => {
-                    tableOfContent[variablesLookup[index]].push(element);
-                });
-            });
-
-            let content = feature.background.content.concat(outline.content);
-
-            return new Array(tableOfContentLength).fill().map((element, index) => {
-                return {
-                    tags,
-                    name: `${name} #${index}`,
-                    content: content.map((line) => {
-                        let resultLine = line;
-                        for (variable in tableOfContent)
-                        {
-                            let variableRegexp = new RegExp(`<${variable}>`,'g');
-                            let variableValue = tableOfContent[variable][index];
-                            resultLine = resultLine.replace(variableRegexp, variableValue);
-                        };
-                        return resultLine;
-                    })
-                };
-            });
-
-        });
-
-    return [].concat(scenarios).concat(...outlines);
-};
 
 console.log(JSON.stringify(testBuilder(featureParser('./features/test.feature')), null, '  '));
 
