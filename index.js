690a79fa110ba14304200c31e98523e2e730affc
Korelin Vadim <korelinvi@gmail.com>
2017-09-28 16:22:21 +0500

    steps importer prototype

--- index.js
+++ index.js
@@ -3,13 +3,40 @@ const testBuilder = require('./lib/testBuilder');
 const testExecutor = require('./lib/testExecutor');
 
 
-function DefineStep(regexp, method) {
+function DefineStep(regexp, method)
+{
     return {
         method,
         regexp,
     };
 };
 
+function GetDefenitions(files)
+{
+    let defenitions = [];
+    let context = {
+        Given: (regexp, method) => defenitions.push(DefineStep(regexp, method)),
+        When: (regexp, method) => defenitions.push(DefineStep(regexp, method)),
+        Then: (regexp, method) => defenitions.push(DefineStep(regexp, method))
+    };
+
+    files.forEach((filePath) => require(filePath).apply(context));
+
+    return defenitions;
+};
+
+let defs = [
+    './steps/given.js',
+    './steps/then.js',
+    './steps/when.bootstrap.js',
+    './steps/when.js',
+    './steps/when.kpgz.js'
+];
+
+console.log(GetDefenitions(defs));
+
+
+/*
 let tests = testBuilder(featureParser('./features/test.feature'));
 
 let sd = [
@@ -17,26 +44,24 @@ let sd = [
         //console.log(user)
     }),
     DefineStep(/^logout$/, function() {
-        console.log(this);
-        console.log(`tid: ${tid}`);
+        //throw 'error thrown';
+        //console.log('logout');
     })
 ];
 let hooks = {
 
-    before: ({tid, context}) => {
-        console.log('before');
-        context.tid = tid;
+    //before: ({tid, context}) => console.log('before'),
+    after: (data) => {
+        console.log(data);
+        //console.log('after');
     },
-    //after: ({tid}) => {
-    //    console.log(tid);
-    //    console.log('after');
-    //},
     //testPassed: () => console.log('testPassed'),
     //testFailed: () => console.log('testFailed'),
-    //beforeStep: () => console.log('beforeStep'),
-    //afterStep: () => console.log('afterStep'),
+    //beforeStep: (data) => {},
+    //afterStep: (data) => console.log(data),
     //stepPassed: () => console.log('stepPassed'),
     //stepFailed: () => console.log('stepFailed')
 
 }
 testExecutor(tests[0], sd, hooks);
+*/
