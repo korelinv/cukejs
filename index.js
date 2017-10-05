b3fa69ffd847242ea486bd923acc836326758c33
Korelin Vadim <korelinvi@gmail.com>
2017-10-05 14:05:07 +0500

    feature parser tests

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const RunBundle = require('./lib/bundleRunner');
 
 module.exports = RunBundle;
+
+
+const parse = require('./lib/featureParser/featureParser')();
+
+console.log(JSON.stringify(parse('test.feature'), null, '  '));
--- test/featureParser.js
+++ test/featureParser.js
@@ -1,9 +1,119 @@
 const assert = require('assert');
+const tokenEnum = require('../lib/featureParser/tokenEnum');
+const getFile = require('../lib/featureParser/getFile');
+const identifyLine = require('../lib/featureParser/identifyLine');
+const featureParser = require('../lib/featureParser/featureParser');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Parse feature', function() {
+
+    describe('#getFile(path)', function() {
+
+        let fs = {
+            readFileSync: () => `
+
+
+                @tag
+
+                Feature: ft
+
+                    Background: bg
+                        Given first
+
+                    Scenario: sc
+                        Then second
+
+                    Scenario Outline:
+                        Then second outline <foo>
+
+                    Examples:
+                        | foo |
+                        | bar |
+
+
+                        `
+        };
+        let get = getFile(fs);
+
+        it('should split file into trimmed lines & strip of empty lines', function() {
+            let expected = [
+                '@tag',
+                'Feature: ft',
+                'Background: bg',
+                'Given first',
+                'Scenario: sc',
+                'Then second',
+                'Scenario Outline:',
+                'Then second outline <foo>',
+                'Examples:',
+                '| foo |',
+                '| bar |'
+            ];
+            let result = get('foo');
+
+            assert.deepStrictEqual(result, expected);
+        });
+
     });
-  });
+
+
+    describe('#featureParser(path)', function() {
+
+        let getFileMock = () => [
+            '@tag',
+            'Feature: ft',
+            'Background: bg',
+            'Given first',
+            'Scenario: sc',
+            'Then second',
+            'Scenario Outline:',
+            'Then second outline <foo>',
+            'Examples:',
+            '| foo |',
+            '| bar |'
+        ];
+        let parse = featureParser(null, null, getFileMock, null);
+
+        it('should return valid feature object', function() {
+            let expected = {
+                tags: [
+                    '@tag'
+                ],
+                name: 'ft',
+                background: {
+                    name: 'bg',
+                    content: [
+                        'first'
+                    ]
+                },
+                scenario: [
+                    {
+                        name: 'sc',
+                        content: [
+                            'second'
+                        ]
+                    }
+                ],
+                outline: [
+                    {
+                        name: '',
+                        content: [
+                            'second outline <foo>'
+                        ],
+                        tableOfContent: [
+                            [
+                                'foo'
+                            ],
+                            [
+                                'bar'
+                            ]
+                        ]
+                    }
+                ]
+            };
+            let result = parse('foo');
+
+            assert.deepStrictEqual(result, expected);
+        });
+    });
+
 });
