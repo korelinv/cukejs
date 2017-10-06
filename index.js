3262faa823483d16178191b1fa72b99f7fc71721
Korelin Vadim <korelinvi@gmail.com>
2017-10-06 14:21:08 +0500

    +

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const RunBundle = require('./lib/bundleRunner');
-
-module.exports = RunBundle;
-
-
+const run = require('./lib/bundleRunner');
 const transpile = require('./lib/featureTranspiler/featureTranspiler')();
 
-transpile('test.feature', {
-    nameBuilder: () => 'foo',
-    indent: true,
-    indentation: '    ',
-    space: true,
-    spacing: 1
-}).map(el => console.log(el));
+module.exports = {run, transpile};
